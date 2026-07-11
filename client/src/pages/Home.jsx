76e93b9ee52304f35fc0bae075187b40356ac4a9
BhumikaCoder21 <bhumikanew21@gmail.com>
2026-07-11 17:20:16 +0530

    fix: correct import path for Banner component in Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Banner from '../components/home/banner'
+import Banner from "../components/home/Banner"
 import Hero from '../components/home/Hero'
 import Features from '../components/home/Features'
 import Testimonial from '../components/home/Testimonial'
@@ -9,8 +9,8 @@ import Footer from '../components/home/Footer'
 const Home = () => {
   return (
     <div>
-          <Banner />
-          <Hero />
+        <Banner />
+        <Hero />
       <Features />
       <Testimonial />
       <CallToAction />
